95e4f88acc45c46f4374c146a9fb00b04dd45ee9
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-15 20:45:35 +0300

    Implemented algorithm.

--- src/com/MIA/Main.java
+++ src/com/MIA/Main.java
@@ -1,9 +1,26 @@
 package com.MIA;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[]  numbers = new int[] {2,3,7,4,8};
+        int target = 7;
+        int[] result =getTwoSum(numbers, target) ;
+        System.out.println(result[0] + " " + result[1]);
     }
 
+    public static int[] getTwoSum(int[] numbers, int target) {
+        Map<Integer, Integer> visitedNumbers = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int delta = target - numbers[i];
+            if (visitedNumbers.containsKey(delta)){
+            return new int[]{i, visitedNumbers.get(delta)};
+            }
+            visitedNumbers.put(numbers[i], i);
+        }
+        return new int[] {-1, -1};
+    }
 }
